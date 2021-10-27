a9c5b6976ef5a3b8999f78968f97512672465f10
Christian Webb <36595425+chriswebbb@users.noreply.github.com>
2021-10-27 23:03:24 +0100

    Update debug.h

--- 4/debug.h
+++ 4/debug.h
@@ -1,11 +1,3 @@
-/*  Author: Steve Gunn
- * Licence: This work is licensed under the Creative Commons Attribution License.
- *          View this license at http://creativecommons.org/about/licenses/
- *   Notes: F_CPU must be defined to match the clock frequency
- *   Usage: Include in your main file and call init_debug_uart0() from the beginning of main
- *          to initialise redirection of stdout, stderr and stdin to UART0.
- */
-
 #define __ASSERT_USE_STDERR
 #include <assert.h>
 #include <stdio.h>
